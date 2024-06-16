0d7539ddd9be8b7bac6ef5bc890a64682d23f784
Makiko Yokoyama <115846240+trimakichan@users.noreply.github.com>
2024-06-16 12:15:20 -0700

    load more button for rendering the new listings is working

--- src/screens/Listings.jsx
+++ src/screens/Listings.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { Link } from "react-router-dom";
 import { Contexts } from "../contexts/contexts";
 import { useFadeInYAxisAnimSettings } from "../animations/animationHooks";
@@ -11,21 +12,29 @@ import { useBoatListings } from "../api/fetchListings";
 import Loading from "../components/Loading/Loading";
 import SearchBar from "../components/SearchBar/SearchBar";
 
+// const MAX_ITEMS_PER_LOAD = 6;
+
 const Listings = () => {
   const { filteredResults } = useContext(Contexts);
+  // const [visibleCount, setVisibleCount] = useState(MAX_ITEMS_PER_LOAD);
+  // const { ref, inView } = useInView({
+  //   triggerOnce: false,
+  //   rootMargin: "100px 0px", // Consider adjusting rootMargin to better fit your use case
+  // });
   const fadeInAnimSettings = useFadeInYAxisAnimSettings();
 
-  const {
-    isLoading,
-    isError,
-    error,
-    isFetching,
-    data: boatListings,
-  } = useBoatListings();
-  // console.log(boatListings);
+  const { isLoading, isError, error, isFetching, data: boatListings } = useBoatListings();
 
+  // const boatListings = data?.slice(0, visibleCount);
 
-  return boatListings ? (
+  // Update visibleCount when the last item comes into view
+  // useEffect(() => {
+  //   if (inView) {
+  //     setVisibleCount((prev) => prev + MAX_ITEMS_PER_LOAD);
+  //   }
+  // }, [inView]);
+
+  return (
     <main className="listings">
       <header className="listings__hero"></header>
 
@@ -39,17 +48,14 @@ const Listings = () => {
             </motion.div>
           </header>
 
-          <div className="sale-filter">
-            {boatListings && (
-              <SearchBar  allBoats={boatListings} />
-            )}
-
-          </div>
-
-          {isLoading && <Loading />}
-
-          {boatListings && (
+          {isError ? (
+            <div className="textMLora">{error}</div>
+          ) : boatListings ? (
             <>
+              <div className="sale-filter">
+                <SearchBar allBoats={boatListings} />
+              </div>
+
               <div className="sale-listings">
                 {/* Always display filteredResults if they exist and contain elements */}
                 {filteredResults &&
@@ -58,8 +64,12 @@ const Listings = () => {
                     <Card key={index} item={boat} />
                   ))}
               </div>
-              <button>Load More</button>
+              {/* <button>Load More</button> */}
+              {/* This div is observed to trigger loading more items */}
+              {/* <div ref={ref} className="load-more-trigger" /> */}
             </>
+          ) : (
+            <Loading />
           )}
         </section>
       </div>
@@ -86,8 +96,6 @@ const Listings = () => {
         </div>
       </article>
     </main>
-  ) : (
-    <Loading />
   );
 };
 
