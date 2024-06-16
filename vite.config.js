6e6461b5f5a4fef29cef91349e4068e653e510e1
Makiko Yokoyama <115846240+trimakichan@users.noreply.github.com>
2024-06-16 13:36:01 -0700

    changed urls for production

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Fairhaven_Yachts/",
+
 
   //remove later
   // server: {
@@ -18,14 +18,14 @@ export default defineConfig({
     // port: 5174, 
 
     //change here before production?
-    proxy: {
-      // Proxying API requests
-      '/api': {
-        target: 'https://api.boats.com', // Target API
-        changeOrigin: true,  // Needed for virtual hosted sites
-        rewrite: (path) => path.replace(/^\/api/, ''),  // Rewrite path removing '/api'
-      }
-    }
+    // proxy: {
+    //   // Proxying API requests
+    //   '/api': {
+    //     target: 'https://api.boats.com', // Target API
+    //     changeOrigin: true,  // Needed for virtual hosted sites
+    //     rewrite: (path) => path.replace(/^\/api/, ''),  // Rewrite path removing '/api'
+    //   }
+    // }
   },
 
   build: {
