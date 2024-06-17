2ab2fd4bb7bb8a70ee989d0ea13148ce05510531
Makiko Yokoyama <115846240+trimakichan@users.noreply.github.com>
2024-06-16 17:35:45 -0700

    edited fetchListing

--- src/api/fetchListings.jsx
+++ src/api/fetchListings.jsx
@@ -1,12 +1,12 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+// const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 // console.log(apiBaseUrl)
 
 const fetchApi = async (url) => {
   // const response = await fetch(`${apiBaseUrl}${url}`);
-  const response = await fetch(`${apiBaseUrl}${url}`);
+     const response = await fetch(url);
 
   if (!response.ok) {
     throw new Error(`Failed to fetch: ${response.statusText}`);
@@ -142,14 +142,16 @@ const filterData = async (results) => {
 
 const fetchBoatListings = async () => {
   // const url = `/inventory/search?key=${apiKey}&status=active,sale%20pending`;
-  const url = `/inventory/search?key=${apiKey}&status=active,sale%20pending`;
+  // const url = `/inventory/search?key=${apiKey}&status=active,sale%20pending`;
+    const url = `https://api.boats.com/inventory/search?key=${apiKey}&status=active,sale%20pending`;
   const results = await fetchApi(url);
   return filterData(results);
 };
 
 const fetchBoatListingById = async (id) => {
   // const url = `/inventory/search?key=${apiKey}&DocumentID=${id}`;
-  const url = `/inventory/search?key=${apiKey}&DocumentID=${id}`;
+  // const url = `/inventory/search?key=${apiKey}&DocumentID=${id}`;
+  const url = `https://api.boats.com/inventory/search?key=${apiKey}&DocumentID=${id}`;
   const results = await fetchApi(url);
   const filteredResults = await filterData(results); // Await the promise resolution
   console.log(filteredResults[0]);
